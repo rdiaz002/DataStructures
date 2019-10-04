7b94b359f70b51f03dc024c31473b7ec79030cab
Ronuel Diaz <r.diazprog@gmail.com>
2019-10-04 16:03:46 -0400

    added time complexity comments

--- DataStructurePractice/Trie.cpp
+++ DataStructurePractice/Trie.cpp
@@ -10,7 +10,7 @@ void Trie::insert(std::string key, int value)
 {
 	TrieNode* current = root;
 	bool found = false;
-	for (auto k = key.begin();k != --key.end(); k++) {
+	for (auto k = key.begin();k != --key.end(); k++) { //Complexity: O(key.size * size of children array) for each letter of the key we must find if the letter exists in the children array
 		if (current->children->find(*k) == current->children->end()) {
 				current->children->insert(std::make_pair(*k, new TrieNode(*(k), 0)));
 				found = false;
@@ -35,7 +35,7 @@ void Trie::insert(std::string key, int value)
 int Trie::search(std::string key)
 {
 	TrieNode * currentNode = root;
-	for (auto k = key.begin(); k != --key.end(); k++) {
+	for (auto k = key.begin(); k != --key.end(); k++) { //Complexity : O(Key.size * Size of children array)
 		if (currentNode->children->find(*k) != currentNode->children->end()) {
 			currentNode = currentNode->children->at(*k);
 		}
